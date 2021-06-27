4d40b83326d870d99e220fe9ebfdcc5af002437e
Ken Butler <ken@swiss-soccer.net>
2021-06-27 16:24:28 -0400

    update theil-sen

--- R/theil_sen_slope.R
+++ R/theil_sen_slope.R
@@ -1,6 +1,7 @@
 #' Theil-Sen slope with time
 #' 
-#' @param x numeric, a time series
+#' @param y numeric, a time series
+#' @param x numeric, the time points of the time series (optional, defaults to 1, 2, ...)
 #' 
 #' @return the Theil-Sen slope, a number
 #' 
@@ -8,14 +9,16 @@
 #' 
 #' @examples 
 #' theil_sen_slope(c(2,3,5,10))
+#' theil_sen_slope(y = c(2,3,5,10), x = 1:4)
+#' theil_sen_slope(y = c(2,3,5,10), x = c(1, 2, 3, 5))
 #' 
 #' @export
 #' 
 theil_sen_slope <-
-function(x) {
-  m1=outer(x,x,"-")
+function(y, x = 1:length(y)) {
+  m1=outer(y,y,"-")
   nn=1:length(x)
-  m2=outer(nn,nn,"-")
+  m2=outer(x,x,"-")
   # elementwise division is almost right, except 0/0 on diag
   m3=m1/m2
   x=m3[lower.tri(m3)]
